173b6120f0b578bcae37037eaa0d835c46b3cdbd
Angelo Zuo <582876716@qq.com>
2018-06-28 16:53:00 +0800

    Refactor codes

--- app/components/customers/CustomerDetail.jsx
+++ app/components/customers/CustomerDetail.jsx
@@ -3,46 +3,38 @@ import PropTypes from "prop-types";
 import { Form, Field } from "formik";
 import { Input, Spin, Alert } from "antd";
 
-import _ from "lodash";
-
 const CustomerDetail = ({ values, isDisabled, loading, children, tableConfig, errors }) => {
-    function getLabel(originLabel) {
-        let formatLabel = "";
-        _.forEach(tableConfig, configValue => {
-            if (configValue.dataIndex === originLabel) {
-                formatLabel = configValue.title;
-            }
-        });
-
-        return formatLabel;
-    }
-
-    let elements = Object.keys(values).map(customerKey => (
-        <Field
-            key={customerKey}
-            name={customerKey}
-            render={({ field }) => {
-                return (
-                    <div style={{ marginBottom: 16 }}>
-                        <Input
-                            {...field}
-                            disabled={isDisabled}
-                            addonBefore={getLabel(customerKey)}
-                            placeholder={`Please enter ${customerKey}`}
-                        />
-                        {typeof errors[customerKey] === "string" ? (
-                            <Alert message={errors[customerKey]} type="error" showIcon />
-                        ) : null}
-                    </div>
-                );
-            }}
-        />
-    ));
+    const getLabel = originLabel =>
+        tableConfig.find(element => element.dataIndex === originLabel).title;
 
     return (
         <Spin spinning={loading}>
             <Form>
-                {elements}
+                {Object.keys(values).map(customerKey => (
+                    <Field
+                        key={customerKey}
+                        name={customerKey}
+                        render={({ field }) => {
+                            return (
+                                <div style={{ marginBottom: 16 }}>
+                                    <Input
+                                        {...field}
+                                        disabled={isDisabled}
+                                        addonBefore={getLabel(customerKey)}
+                                        placeholder={`Please enter ${customerKey}`}
+                                    />
+                                    {typeof errors[customerKey] === "string" ? (
+                                        <Alert
+                                            message={errors[customerKey]}
+                                            type="error"
+                                            showIcon
+                                        />
+                                    ) : null}
+                                </div>
+                            );
+                        }}
+                    />
+                ))}
                 {children}
             </Form>
         </Spin>
